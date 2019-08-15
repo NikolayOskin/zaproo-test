f3d65ef4418c60d2b82b130e4ba9f1673c39a532
Oskin <wskeal@gmail.com>
2019-08-16 01:25:08 +0300

    search implementation

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -2,15 +2,25 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Http\Requests\SearchProduct;
 use App\Http\Requests\StoreProduct;
 use App\Http\Requests\UpdateProduct;
 use App\Models\Product;
 use App\Services\ProductSearchService;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+/**
+ * @group Products
+ *
+ * APIs for products
+ */
 class ProductController extends Controller
 {
+    /**
+     * Get all products
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function index()
     {
         $products = Product::with('productStatus')->get();
@@ -18,6 +28,15 @@ class ProductController extends Controller
         return response()->json(['success' => $products], 200);
     }
 
+    /**
+     * Store product
+     *
+     * @bodyParam name string required Alphanumeric
+     * @bodyParam price float required Two decimals number
+     * @bodyParam in_stock boolean required 1 or 0, not "true" or "false
+     * @param StoreProduct $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function store(StoreProduct $request)
     {
         $product = Product::create($request->all());
@@ -26,6 +45,16 @@ class ProductController extends Controller
         return response()->json(['success' => 'Product created'], 200);
     }
 
+    /**
+     * Update product
+     *
+     * @bodyParam name string Alphanumeric
+     * @bodyParam price float Two decimals number
+     * @bodyParam in_stock boolean 1 or 0, not "true" or "false
+     * @param UpdateProduct $request
+     * @param int $id
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function update(UpdateProduct $request, int $id)
     {
         $product = Product::find($id);
@@ -38,6 +67,12 @@ class ProductController extends Controller
         return response()->json(['success' => 'Product updated'], 200);
     }
 
+    /**
+     * Delete product
+     *
+     * @param int $id
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function destroy(int $id)
     {
         Product::destroy($id);
@@ -45,8 +80,19 @@ class ProductController extends Controller
         return response()->json(['success' => 'Deleted'], 200);
     }
 
-    public function search(Request $request, ProductSearchService $searchService)
+    /**
+     * Search product
+     *
+     * @queryParam term Example: api/v1/products/search?term=iphone7
+     * @queryParam in_stock Example: api/v1/products/search?term=iphone7&in_stock=1 (1 or 0)
+     * @param SearchProduct $request
+     * @param ProductSearchService $productSearchService
+     * @return void
+     */
+    public function search(SearchProduct $request, ProductSearchService $productSearchService)
     {
-        $products = $searchService->search($request);
+        $products = $productSearchService->search($request);
+
+        return response()->json(['success' => $products->get()], 200);
     }
 }
--- app/Services/ProductSearchService.php
+++ app/Services/ProductSearchService.php
@@ -2,12 +2,25 @@
 
 namespace App\Services;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductSearchService
 {
     public function search(Request $request)
     {
-        $results =
+        $products = Product::with('productStatus');
+
+        if ($request->has('in_stock') && !empty($request->in_stock)) {
+            $products = Product::whereHas('productStatus', function ($query) use ($request) {
+                $query->where('in_stock', '=', $request->in_stock);
+            });
+        }
+
+        if ($request->has('term') && $request->term) {
+            $products->where('name', 'like', "%{$request->term}%");
+        }
+
+        return $products;
     }
 }
